b41cf14c0b8b520534c9fb785eb5036fd9349e65
Selenophilia <pauloargenal@gmail.com>
2021-08-02 22:16:11 +0800

    Adds label and adjust stylings

--- src/components/AppPurchase/index.js
+++ src/components/AppPurchase/index.js
@@ -9,8 +9,10 @@ const AppPurchase = () => {
     <div className="container">
       <form className="purchase-form" onSubmit={handleSubmit}>
         <span className="title">Buy Stock</span>
-        <input className="ticker" type="text" placeholder="enter symbol" />
-        <input className="quantiy" type="number" placeholder="enter quantity" />
+        <label className="form-label">Ticker Symbol:</label>
+        <input className="ticker" type="text" />
+        <label className="form-label">Quantity:</label>
+        <input className="quantiy" type="number" />
         <button className="buy-btn">Buy</button>
       </form>
     </div>
