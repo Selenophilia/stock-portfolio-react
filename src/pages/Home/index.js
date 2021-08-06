51840b02e19b5a2c1fe2ef009c4d78160d75b906
Selenophilia <pauloargenal@gmail.com>
2021-08-06 15:06:06 +0800

    Adds mock data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,13 +6,25 @@ import Tab from '../../components/NavigationTabs/Tab';
 import './index.scss';
 
 const Home = () => {
+  function createData(symbol, change, quantity, price, cost) {
+    return { symbol, change, quantity, price, cost };
+  }
+
+  const rows = [
+    createData('AAPL', '-0.24%', '1 share', '150.99', '25.00'),
+    createData('MSFT', '-0.24%', '1 share', '150.99', '25.00'),
+    createData('GOOG', '-0.24%', '1 share', '150.99', '25.00'),
+    createData('AAPL', '-0.24%', '1 share', '150.99', '25.00'),
+    createData('AAPL', '-0.24%', '1 share', '150.99', '25.00')
+  ];
+
   return (
     <div className="stock-container">
       <AppHeader />
       <div className="stock-content">
-        <AppPurchase />
+        <AppPurchase rows={rows} createData={createData} />
         <div className="tabs">
-          <TabControl>
+          <TabControl rows={rows} createData={createData}>
             <Tab label="Portfolio" />
             <Tab label="Transactions" />
             <Tab label="Market Summary" />
