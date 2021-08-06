20edcd3d53cc4339dff5d011fe32db791077e2c1
Selenophilia <pauloargenal@gmail.com>
2021-08-06 15:05:35 +0800

    Modifies tab control

--- src/components/NavigationTabs/TabControl.js
+++ src/components/NavigationTabs/TabControl.js
@@ -5,16 +5,17 @@ import MarketSummary from '../MarketSummary';
 import Portfolio from '../Portfolio';
 import Transactions from '../Transactions';
 import './tabs.scss';
-const TabControl = ({ children }) => {
+const TabControl = ({ rows, children }) => {
   const labels = [];
   const [activeTab, setActivetab] = useState(children[0].props.label);
 
   const changeTab = (tab) => {
     setActivetab(tab);
   };
+
   const handleContent = (label) => {
     if (label === 'Portfolio') {
-      return <Portfolio />;
+      return <Portfolio rows={rows} />;
     } else if (label === 'Transactions') {
       return <Transactions />;
     } else if (label === 'Market Summary') {
@@ -33,6 +34,7 @@ const TabControl = ({ children }) => {
 };
 
 TabControl.propTypes = {
+  rows: Proptypes.array.isRequired,
   children: Proptypes.array.isRequired
 };
 
