0a0893d2cf2286cbd755667d4af28d7ea9888c08
Selenophilia <pauloargenal@gmail.com>
2021-08-12 00:06:34 +0800

    Adds material ui design for sign in and sign up

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,14 +1,63 @@
 import { useMutation } from '@apollo/client';
 import React, { useRef, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { login } from '../../api/mutation';
 import AuthContext from '../../contexts/AuthContext';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import './index.scss';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh'
+  },
+  image: {
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1621264448270-9ef00e88a935?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=608&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#1467af'
+  }
+}));
+
 const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const history = useHistory();
   const { setAuth } = useContext(AuthContext);
+
+  const classes = useStyles();
 
   const [loginFunc] = useMutation(login, {
     onCompleted: (data) => {
@@ -28,26 +77,65 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
-      <form className="loginform" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="email address"
-          className="email"
-          ref={emailRef}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="password"
-          ref={passwordRef}
-        />
-        <button className="signin">Sign in</button>
-        <p className="message">
-          Not registered? <Link to="/register">Create an account</Link>
-        </p>
-      </form>
-    </div>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              inputRef={emailRef}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              inputRef={passwordRef}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register">{"Don't have an account? Sign Up"}</Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,15 +1,64 @@
 import React, { useRef, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { register } from '../../api/mutation';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import AuthContext from '../../contexts/AuthContext';
 import './index.scss';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh'
+  },
+  image: {
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1621264448270-9ef00e88a935?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=608&q=80)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#1467af'
+  }
+}));
+
 const Register = () => {
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const history = useHistory();
   const { setAuth } = useContext(AuthContext);
+
+  const classes = useStyles();
 
   const [registerFunc] = useMutation(register, {
     onCompleted: (data) => {
@@ -30,32 +79,71 @@ const Register = () => {
   };
 
   return (
-    <div className="register">
-      <form className="registerform" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="user name"
-          className="name"
-          ref={nameRef}
-        />
-        <input
-          type="text"
-          placeholder="email address"
-          className="email"
-          ref={emailRef}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          className="password"
-          ref={passwordRef}
-        />
-        <button className="register">register</button>
-        <p className="message">
-          Already have an account? <Link to="/login">Login</Link>
-        </p>
-      </form>
-    </div>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <PermContactCalendarIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="User name"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              inputRef={nameRef}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              inputRef={emailRef}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              inputRef={passwordRef}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="#primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/login">{'Already have an account? Sign Ip'}</Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
