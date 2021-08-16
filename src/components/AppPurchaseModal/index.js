42aca15090fff48f8381b81c03fb3f339b1d05f2
Selenophilia <pauloargenal@gmail.com>
2021-08-16 19:37:32 +0800

    Modifies puchase mutation

--- src/components/AppPurchaseModal/index.js
+++ src/components/AppPurchaseModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Avatar,
@@ -10,6 +10,7 @@ import {
   TextField
 } from '@material-ui/core';
 import PaymentIcon from '@material-ui/icons/Payment';
+import { fetchSymbols, fetchQuote } from '../../api';
 import { useMutation } from '@apollo/client';
 import { purchase } from '../../api/mutation';
 
@@ -30,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     height: 550,
     width: 450,
     top: '10%',
-    marginLeft: 500,
+    marginLeft: 650,
     backgroundColor: theme.palette.background.paper,
     border: 'none',
     borderRadius: 12,
@@ -84,72 +85,73 @@ const useStyles = makeStyles((theme) => ({
 export const AppPurchaseModal = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-
+  const [symbols, setSymbols] = useState(null);
+  const [errMsg, setErrMsg] = useState(null);
   const symbolRef = useRef();
   const quantiyRef = useRef();
 
   const [purchaseFunc] = useMutation(purchase, {
     onCompleted: (data) => {
-      console.log('data', data);
+      console.log('purrchase', data);
     }
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    purchaseFunc({
-      variables: {
-        symbol: symbolRef.current.value,
-        price: 150.23,
-        openPrice: 50.12,
-        purchasePrice: 142.2,
-        purchaseQuantity: Number(quantiyRef.current.value)
-      }
-    });
+  const fetch = async () => {
+    const data = await fetchSymbols();
+    return data;
   };
 
+  const validateData = (input) => {
+    const results = symbols
+      .map((data) => {
+        return data.symbol;
+      })
+      .find((values) => {
+        if (values === input) {
+          return values;
+        } else {
+          setErrMsg('Please include a valid symbol');
+        }
+      });
+
+    return results;
+  };
+  useEffect(() => {
+    fetch().then((symbol) => {
+      setSymbols(symbol);
+    });
+  }, []);
   const handleOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
+    setErrMsg(null);
   };
 
-  const body = (
-    <div className={classes.paper}>
-      <Typography variant="h5" component="h2" className={classes.title}>
-        Buy a stock
-      </Typography>
-      <Avatar className={classes.avatar}>
-        <PaymentIcon fontSize="large" />
-      </Avatar>
-      <Divider variant="middle" className={classes.divider} />
-      <form className="purchase-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <TextField
-            id="outlined-basic"
-            label="Ticker Symbol"
-            className={classes.field}
-            variant="outlined"
-            inputRef={symbolRef}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Quantity"
-            className={classes.field}
-            variant="outlined"
-            inputRef={quantiyRef}
-          />
-        </div>
-        <Box component="div" className={classes.buttons}>
-          <Button className={classes.cancel} onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button className={classes.confirm}> Confirm </Button>
-        </Box>
-      </form>
-    </div>
-  );
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const validSymbol = validateData(symbolRef.current.value);
+      if (validSymbol) {
+        const { latestPrice, iexOpen } = await fetchQuote(validSymbol);
+        const total = quantiyRef.current.value * latestPrice;
+        purchaseFunc({
+          variables: {
+            symbol: symbolRef.current.value,
+            price: latestPrice,
+            openPrice: iexOpen,
+            purchasePrice: total,
+            purchaseQuantity: Number(quantiyRef.current.value)
+          }
+        });
+        handleClose();
+      }
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
 
   return (
     <>
@@ -167,7 +169,49 @@ export const AppPurchaseModal = () => {
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        {body}
+        <div className={classes.paper}>
+          <Typography variant="h5" component="h2" className={classes.title}>
+            Buy a stock
+          </Typography>
+          <Avatar className={classes.avatar}>
+            <PaymentIcon fontSize="large" />
+          </Avatar>
+          <Divider variant="middle" className={classes.divider} />
+          {errMsg ? (
+            <Typography variant="h6" component="h6">
+              {errMsg}
+            </Typography>
+          ) : (
+            ''
+          )}
+          <form className="purchase-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Ticker Symbol"
+                className={classes.field}
+                variant="outlined"
+                inputRef={symbolRef}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Quantity"
+                className={classes.field}
+                variant="outlined"
+                inputRef={quantiyRef}
+                type="number"
+              />
+            </div>
+            <Box component="div" className={classes.buttons}>
+              <Button className={classes.cancel} onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button className={classes.confirm} type="submit">
+                Confirm
+              </Button>
+            </Box>
+          </form>
+        </div>
       </Modal>
     </>
   );
